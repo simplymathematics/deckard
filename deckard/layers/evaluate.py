e1289bf89bafeeb1f92a883ce688543dc76df83d
simplymathematics <15224968+simplymathematics@users.noreply.github.com>
2021-12-01 04:22:22 +0100

    dvc repro bug fix

--- deckard/layers/evaluate.py
+++ deckard/layers/evaluate.py
@@ -3,14 +3,17 @@ from deckard.base import Data, Experiment, Model
 from os import path
     
 if __name__ == '__main__':
+    from time import process_time
+    start = process_time()
     import argparse
     import logging
+    
 
     # command line arguments
     parser = argparse.ArgumentParser(description='Evaluate a model')
     parser.add_argument('-f', '--folder', type=str, help='Folder containing the checkpoint.', required=True)
-    parser.add_argument('--scorer', type=str, default='f1', help='Scorer string.')
-    parser.add_argument('--verbosity', type=str, default=None, help='Verbosity level.')
+    parser.add_argument('-s', '--scorer', type=str, default='f1', help='Scorer string.')
+    parser.add_argument('--verbosity', type=str, default='INFO', help='Verbosity level.')
     parser.add_argument('--batch_size', type=int, default=32, help='Batch size.')
     parser.add_argument('--output', type=str, help='Output file.')
     args = parser.parse_args()
@@ -32,7 +35,12 @@ if __name__ == '__main__':
     assert isinstance(model, Model), 'model is not a valid Model object.'
     experiment = Experiment(data = data, model = model)
     assert isinstance(experiment, Experiment), 'experiment is not a valid Experiment object.'
+    
     experiment.run()
+    end = process_time()
+    logging.info('Evaluation took {} seconds.'.format(end - start))
+    logging.info('Number of evaluated sample: {}'.format(len(experiment.data.y_test)))
+    logging.info('Time per sample: {} seconds'.format(round((end - start) / len(experiment.data.y_test),3)))
     print(experiment.scores[args.scorer.upper()])
     logging.debug(experiment.scores)
 
