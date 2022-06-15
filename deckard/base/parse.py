2408bc55f0a3267d72a6b8e646cd69cff7850dc4
Charlie Meyers <cmeyers@cs.umu.se>
2022-06-15 14:49:32 +0000

    added data  yml parser to parse.py

--- deckard/base/parse.py
+++ deckard/base/parse.py
@@ -27,16 +27,21 @@ def generate_tuple_list_from_yml(filename:str) -> list:
     for entry in yml_list:
         if not isinstance(entry, dict):
             raise ValueError("Error parsing yml file {}".format(filename))
+        special_keys = {}
         for key, value in entry['params'].items():
             if isinstance(value, (tuple, float, int, str)):
                 special_values = value
                 special_key = key
+                special_keys[special_key] = special_values
+        for key in special_keys.keys():
+            entry['params'].pop(key)
         grid = ParameterGrid(entry['params'])
         name = entry['name']
-        for param in grid:
-            if "special_key" in locals() and special_key in param:
-                param[special_key] = special_values
-            full_list.append((name, param))
+        for combination in grid:
+            if "special_keys" in locals():
+                for key, value in special_keys.items():
+                    combination[key] = value
+            full_list.append((name, combination))
     return full_list
 
 def generate_object_list_from_tuple(yml_tuples:list, *args) -> list:
@@ -82,7 +87,36 @@ def generate_experiment_list(model_list:list, data, cv = None, model_type = 'skl
         model = model
         if not isinstance(model, Model):
             model = Model(model, model_type = model_type)
-        experiment = Experiment(data, model)
+        experiment = Experiment(data = data, model = model)
         experiment_list.append(experiment)
     return experiment_list    
 
+def parse_data_from_yml(filename:str) -> dict:
+    assert isinstance(filename, str)
+    LOADER = yaml.FullLoader
+    # check if the file exists
+    params = dict()
+    if not path.isfile(str(filename)):
+        raise ValueError(str(filename) + " file does not exist")
+    # read the yml file
+    with open(filename, 'r') as stream:
+        try:
+            data_file = yaml.load(stream, Loader=LOADER)[0]
+            logger.info(data_file)
+            logger.info(type(data_file))
+        except yaml.YAMLError as exc:
+            raise ValueError("Error parsing yml file {}".format(filename))
+    # check that datas is a list
+    if not isinstance(data_file, dict):
+        raise ValueError("Error parsing yml file {}. It must be a yaml dictionary.".format(filename))
+    params = data_file['params']
+    data_name = data_file['name']
+    logger.info(f"Parsing data from {filename}")
+    logger.info(f"Data name: {data_name}")
+    logger.info(f"Data params: {params}")
+    for param, value in params.items():
+        logger.info(param + ": " + str(value))
+    data = Data(data_name, **params)
+    assert isinstance(data, Data)
+    logger.info("{} successfully parsed.".format(filename))
+    return data
