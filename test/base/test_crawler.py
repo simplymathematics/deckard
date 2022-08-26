f345882cfe84bc31f87cb819909aa28b58041f55
Charlie Meyers <cmeyers@cs.umu.se>
2022-08-26 14:37:37 +0000

    cleanup

--- test/base/test_crawler.py
+++ test/base/test_crawler.py
@@ -1,27 +1,13 @@
 import warnings
+import unittest
+from deckard.base.crawler import Crawler, crawler_config
+from os import path, remove
+from pandas import DataFrame
 from sklearn.exceptions import UndefinedMetricWarning
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
-import tempfile
-import unittest
-from deckard.base import Data, Model, Experiment
-from deckard.base.crawler import Crawler, crawler_config
-import numpy as np
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.cluster import KMeans
-from sklearn.metrics import accuracy_score
-from sklearn.neighbors import  KNeighborsClassifier, KNeighborsRegressor
-from sklearn.impute import SimpleImputer
-from copy import deepcopy
-from collections.abc import Callable
-from art.attacks.evasion import BoundaryAttack
-from art.defences.preprocessor import FeatureSqueezing
-from art.estimators.classification.scikitlearn import ScikitlearnClassifier
-from art.defences.preprocessor import FeatureSqueezing
-from art.defences.postprocessor import HighConfidence
-from os import path, remove
-from pandas import DataFrame
+
 
 class testCrawler(unittest.TestCase):
     def setUp(self):
