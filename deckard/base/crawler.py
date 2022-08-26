8309546f6a75a9dd98cc715cad210fde1eb6aeeb
Charlie Meyers <cmeyers@cs.umu.se>
2022-08-26 14:35:13 +0000

    better cleaning and testing

--- deckard/base/crawler.py
+++ deckard/base/crawler.py
@@ -1,28 +1,37 @@
-import json, os
+import numpy as np
 import pandas as pd
+import os, logging, json, yaml
+from deckard.base import Experiment, Model, Data
+from deckard.base.scorer import Scorer
 
 crawler_config = {
     "filenames" : [
         'data_params', 'defence_params', 'experiment_params', 'model_params',
         'attack_params', 'predictions', 'adversarial_predictions', 'adversarial_scores', 'scores', 
         'time_dict'
-    ]
+    ],
+    "filetypes" : ['json'],
+    "output" : 'data.csv',
+    "path" :  [
+            'root', 'path', 'data', 'directory', 'layer', 'defence_id', 'attack_id'
+        ],
 }
-
+logger = logging.getLogger(__name__)
 class Crawler():
-    def __init__(self, config_file, path, output):
-        self.config_file = config_file
+    def __init__(self, config, path, output):
+        self.config = config
         self.path = path
         self.output = output
         self.config = crawler_config
         self.data = None
     
     def __call__(self, filetypes = ['csv']):
-        self.crawl_tree()
-        self.clean_data()
+        data = self.crawl_tree()
+        data = self.clean_data(data)
         self.save_data()
+        return self.data
 
-    def crawl_folder(self, path = None):
+    def _crawl_folder(self, path = None):
         if path is None:
             path = self.path
         data = {}
@@ -40,44 +49,127 @@ class Crawler():
         data = {}
         tuples = os.walk(self.path)
         dir_list = [x[0] for x in tuples]
-        good_dirs = [x for x in dir_list if "scores.json" in os.listdir(x)]
+        good_dirs = [x for x in dir_list if "scores.json"  in os.listdir(x)]
+        adv_dirs = [x for x in dir_list if "adversarial_scores.json" in os.listdir(x)]
         for dir in good_dirs:
-            data[dir] = self.crawl_folder(dir)
+            data[dir] = self._crawl_folder(dir)
+        for dir in adv_dirs:
+            data[dir] = self._crawl_folder(dir)
         return data
-        
+    def _explode_params(self, df:pd.DataFrame):
+        new = pd.DataFrame()
+        for col in df.columns:
+            if isinstance(df[col][0], dict) and 'predictions' not in str(col):
+                exploded = pd.DataFrame(pd.json_normalize(df[col]))
+                exploded.index = df.index
+                new_cols = []
+                for col2 in exploded.columns:
+                    if 'experiment' == str(col2):
+                        col2 == 'id'
+                    new_col = col + '_' + col2
+                    new_cols.append(new_col)
+                exploded.columns = new_cols
+                new = pd.concat([new, exploded], axis = 1)
+            elif 'predictions' in str(col):
+                pass
+            else:
+                new[col] = df[col]
+        return new
+
+    def _merge_data(self, data:pd.DataFrame):
+
+        # Drop This From Results
+        try:
+            data.drop(['predictions'], axis = 1, inplace = True)
+        except KeyError:
+            pass
+        try:
+            data.drop(['adversarial_predictions'], axis = 1, inplace = True)
+        except KeyError:
+            pass
+        # split into adv/ben
+        try:
+            attack_data = data[data.adversarial_scores.notna()]
+        except AttributeError:
+            attack_data = data[data.scores.isna()]
+        try:
+            defense_data = data[data.scores.notna()]
+        except AttributeError:
+            defense_data = data[data.adversarial_scores.isna()]
+        # Replace 'experiment' with '_id'
+        attack_data.columns = attack_data.columns.str.replace('_experiment', '_id')
+        defense_data.columns = defense_data.columns.str.replace('_experiment', '_id')
+        # Drop columns
+        attack_data = attack_data.dropna(axis = 1)
+        defense_data = defense_data.dropna(axis = 1)
+        # # Explode params
+        # attack_data = self._explode_params(attack_data)
+        # defense_data = self._explode_params(defense_data)
+        # Parse attack data from path
+        attack_paths = pd.Series(attack_data.index).str.split(os.sep)
+        attack_paths = pd.DataFrame(attack_paths.values.tolist())
+        no_cols = attack_paths.shape[1]
+        attack_paths.columns = self.config['path'][-no_cols:]
+        attack_data = attack_data.reset_index()
+        attack_paths = attack_paths.reset_index()
+        attack_data = pd.concat([attack_data, attack_paths], axis = 1)
+        # Parse Defense Data from path
+        defense_paths = pd.Series(defense_data.index).str.split(os.sep)
+        defense_paths = pd.DataFrame(defense_paths.values.tolist())
+        no_cols = defense_paths.shape[1]
+        defense_path_cols = self.config['path'][-no_cols:]
+        defense_paths.columns = defense_path_cols
+        defense_data.reset_index(inplace = True)
+        defense_paths.reset_index(inplace = True)
+        defense_data = pd.concat([defense_data, defense_paths], axis = 1)
+        # Drop duplicate columns
+        attack_data = attack_data.loc[:,~attack_data.columns.duplicated()]
+        defense_data = defense_data.loc[:,~defense_data.columns.duplicated()]
+        # Merge attack and defense data
+        df = defense_data.merge(attack_data, on = 'defence_id', how = 'outer')
+        df = df.drop([x for x in df.columns if '_y' in str(x)], axis = 1)
+        df.columns = [str(x).replace("_x", "") for x in df.columns]
+        df.columns = [str(x).replace("adversarial", "adv") for x in df.columns]
+        return df
     def clean_data(self, data):
         self.data = {}
         for dir in data.keys():
-            scores = data[dir]['scores']
-            self.data[dir] = scores
+            self.data[dir] = {}
             for filename in self.config['filenames']:
                 if os.path.isfile(os.path.join(dir, filename + '.json')):
-                    if filename == 'adversarial_scores':
-                        adv_scores = data[dir]['adversarial_scores']
-                        new_scores = {}
-                        for key in adv_scores.keys():
-                            new_name = "adv_" + key
-                            new_scores[new_name] = adv_scores[key]
-                        self.data[dir] = new_scores
-                    self.data[dir][filename] = os.path.join(dir, filename + '.json')
+                    value = data[dir][filename]
+                    self.data[dir][filename] = value
         self.data = pd.DataFrame(self.data).T
+        self.data = self._merge_data(self.data).drop('index', axis = 1)
         return self.data
 
-    def save_data(self, data:pd.DataFrame = None, filetype = 'json'):
+    def save_data(self, data:pd.DataFrame = None):
+        try:
+            filetype = self.output.split('.')[-1]
+        except:
+            logger.warning('No filetype detected. Defaulting to csv')
+            filetype = 'csv'
         if data is None:
             data = self.data
         assert isinstance(data, pd.DataFrame)
         if filetype == 'json':
-            filename = os.path.join(self.output + '.json')
-            data.to_json(filename)
+            data.to_json(self.output)
         elif filetype == 'csv':
-            filename = os.path.join(self.output + '.csv')
-            data.to_csv(filename)
+            data.to_csv(self.output)
         elif filetype == 'tsv':
             pass
         elif filetype == 'db':
             pass
         else:
             raise ValueError("Filetype not supported.")
-        return filename
-
+        return self.output
+    
+    def __call__(self, path = None):
+        if path is None:
+            data = self.crawl_tree()
+        else:
+            data = self.crawl_tree(path)
+        data = self.clean_data(data)
+        logger.debug("Saving file to {}".format(self.output))
+        self.save_data(data)
+        return self.data
--- test/base/test_crawler.py
+++ test/base/test_crawler.py
@@ -5,7 +5,8 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
 import tempfile
 import unittest
-from deckard.base import Data, Model, Experiment, Crawler
+from deckard.base import Data, Model, Experiment
+from deckard.base.crawler import Crawler, crawler_config
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cluster import KMeans
@@ -19,80 +20,30 @@ from art.defences.preprocessor import FeatureSqueezing
 from art.estimators.classification.scikitlearn import ScikitlearnClassifier
 from art.defences.preprocessor import FeatureSqueezing
 from art.defences.postprocessor import HighConfidence
-from os import path, listdir
+from os import path, remove
 from pandas import DataFrame
 
 class testCrawler(unittest.TestCase):
     def setUp(self):
-        self.path = tempfile.mkdtemp(dir = "/tmp", prefix='deckard_', suffix = "")
-        ART_DATA_PATH = self.path
-        self.file = 'test_filename'
-        data = Data('iris', test_size = 30)
-        self.assertIsInstance(data, Data)
-        model = Model(KNeighborsRegressor(), model_type = 'sklearn', path = path.join(self.path, 'regressor'))
-        self.assertIsInstance(model, Model)
-        experiment = Experiment(data = data, model = model)
-        experiment.run(filename = 'scores.json', path = path.join(self.path, 'regressor'))
-        model2 = Model(KNeighborsClassifier(), model_type = 'sklearn', path = path.join(self.path, 'classifier'))
-        experiment2 = Experiment(data = data, model = model2)
-        experiment2.run(filename = 'scores.json', path = path.join(self.path, 'classifier'))
-        
+        self.path = "../data/"
+        self.file = "../data/tmp_results.csv"
+        self.config = crawler_config
 
     def test_crawler(self):
-        tmp1 = path.join(self.path, 'regressor')
-        crawler = Crawler(config_file = 'config.yml', path = path.join(self.path, 'regressor'), output = self.file)
-        result = crawler.crawl_folder()
-        self.assertIsInstance(result, dict)
-        self.assertIsInstance(result['scores'], dict)
-        self.assertIsInstance(result['scores']['MAPE'], float)
-        self.assertIsInstance(result['scores']['MSE'], float)
-        self.assertIsInstance(result['scores']['MAE'], float)
-        self.assertIsInstance(result['scores']['R2'], float)
-
-        crawler2 = Crawler(config_file = 'config.yml', path = path.join(self.path, 'classifier'), output = self.file)
-        result2 = crawler2.crawl_folder()
-        self.assertIsInstance(result2, dict)
-        self.assertIsInstance(result2['scores'], dict)
-        self.assertIsInstance(result2['scores']['F1'], float)
-        self.assertIsInstance(result2['scores']['ACC'], float)
-        self.assertIsInstance(result2['scores']['PREC'], float)
-        self.assertIsInstance(result2['scores']['REC'], float)
-        self.assertIsInstance(result2['scores']['AUC'], float)
-        
+        crawler = Crawler(config = self.config, path = self.path, output = self.file)
+        self.assertIsInstance(crawler, Crawler)
+        self.assertIs(crawler.data, None)
+        self.assertIsInstance(crawler.config, dict)
+        self.assertIsInstance(crawler.path, str)
+        self.assertEqual(crawler.output, self.file)
     
-    def test_crawl_tree(self):
-        crawler3 = Crawler(config_file = 'config.yml', path = self.path, output = self.file)
-        result3 = crawler3.crawl_tree()
-        self.assertIsInstance(result3, dict)
-        self.assertEqual(len(list(result3)), 2)
-        for result_dict in result3.keys():
-            tmp = result3[result_dict]
-            self.assertIsInstance(tmp, dict)
-            self.assertIsInstance(tmp['scores'], dict)
-            self.assertIsInstance(tmp['data_params'], dict)
-            self.assertIsInstance(tmp['model_params'], dict)
-    
-    def test_clean_data(self):
-        crawler = Crawler(config_file = 'config.yml', path = self.path, output = self.file)
-        result = crawler.crawl_tree()
-        clean_result = crawler.clean_data(result)
-        self.assertIsInstance(clean_result, DataFrame)
-        self.assertEqual(len(clean_result), 2)
-        self.assertEqual(len(clean_result.columns), 17)
+    def test_crawl_folder(self):
+        c1 = Crawler(config = self.config, path = self.path, output = self.file)
+        d1 = c1()
+        self.assertIsInstance(d1, DataFrame)
+        print(c1.output)
+        self.assertTrue(path.isfile(c1.output))
+        remove(self.file)
 
-    def test_save_data(self):
-        crawler = Crawler(config_file = 'config.yml', path = self.path, output = self.file)
-        crawler2 = Crawler(config_file = 'config.yml', path = self.path, output = self.file)
-        result = crawler.crawl_tree()
-        clean_result = crawler.clean_data(result)
-        json_file = crawler.save_data(clean_result, filetype = 'json')
-        self.assertTrue(path.exists(json_file))
-        csv_file = crawler2.save_data(clean_result, filetype = 'csv')
-        self.assertTrue(path.exists(csv_file))
-
-    
-    def tearDown(self) -> None:
-        from shutil import rmtree
-        rmtree(self.path)
-        del self.path
-        del self.file
+    def tearDown(self):
+        pass
