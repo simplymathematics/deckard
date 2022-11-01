d028b48913e9a0c0d9e8beb93b6eb51cbcb41baf
Charlie Meyers <cmeyers@cs.umu.se>
2022-11-01 21:19:48 +0100

    hash bug

--- examples/tikho/hash.py
+++ examples/tikho/hash.py
@@ -2,7 +2,7 @@ import hashlib
 import subprocess
 from pathlib import Path
 import dvc.api
-
+from os import rename
 if __name__ == '__main__':
     params = dvc.api.params_show()
     path = Path(params["hash"]["out"])
@@ -19,8 +19,8 @@ if __name__ == '__main__':
     path = path / unique_id
     path.mkdir(exist_ok=True, parents=True)
     print(f"Moving folder from {report_path} to {path}")
-    subprocess.run(["mv", f"{report_path}/*", f"{path}"])
+    rename(report_path, path)
     print(f"Moving file from {filename} to {path}")
-    subprocess.run(["cp", "-v", filename, path ], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    rename(filename, path / filename)
     print(f"Rendering plots in {path}/index.html")
     subprocess.run(["dvc", "plots", "show", "-o", path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
