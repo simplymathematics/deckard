414c1d91d9018af9232e2b114dbf7a8177a36b5c
Charlie Meyers <cmeyers@cs.umu.se>
2022-11-01 21:42:20 +0100

    + plot template

--- examples/tikho/hash.py
+++ examples/tikho/hash.py
@@ -2,7 +2,17 @@ import hashlib
 import subprocess
 from pathlib import Path
 import dvc.api
-from os import rename
+from os import rename, rmdir
+
+
+
+html_template = """<!DOCTYPE html>
+
+
+
+"""
+
+
 if __name__ == '__main__':
     params = dvc.api.params_show()
     path = Path(params["hash"]["out"])
@@ -24,4 +34,9 @@ if __name__ == '__main__':
     print(f"Moving file from {filename} to {path}")
     rename(filename, path / filename)
     print(f"Rendering plots in {path}/index.html")
-    subprocess.run(["dvc", "plots", "show", "-o", path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(["dvc", "plots", "show", "-o", path, "--html-template", "template.html"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # delete report_path
+    rmdir(report_path)
+    assert Path(path / "index.html").exists(), "Plots were not rendered"
+    assert Path(path / filename).exists(), "Params was not saved"
+    assert not report_path.exists(), "Report path was not deleted"
