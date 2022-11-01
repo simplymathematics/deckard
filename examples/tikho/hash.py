10f293b0fb84f70175bb2165a55806ba30ad3239
Charlie Meyers <cmeyers@cs.umu.se>
2022-11-01 23:28:59 +0100

    +hash

--- examples/tikho/hash.py
+++ examples/tikho/hash.py
@@ -2,9 +2,10 @@ import hashlib
 import subprocess
 from pathlib import Path
 import dvc.api
-from os import rename, rmdir
+from os import rename
 from shutil import copy2 as copy
 from shutil import rmtree
+
 if __name__ == '__main__':
     params = dvc.api.params_show()
     path = Path(params["hash"]["out"])
@@ -26,22 +27,20 @@ if __name__ == '__main__':
         print("Already exists. Removing old files")
         rmtree(path)
     path.mkdir(exist_ok=True, parents=True)
+    results.append(Path(report_path, "report.html"))
     new_results = [path / result.name for result in results]
+    
     print("Moving results to new location")
     for result, new_result in zip(results, new_results):
         new_result.parent.mkdir(exist_ok=True, parents=True)
         print(f"Moving {result} to {new_result}")
         copy(result, new_result)
-    run_time = [Path(report_path, "scalars"), Path(report_path, "report.html")]
+    run_time = [Path(report_path, "scalars"), ]
     new_run_time = [path / run.name for run in run_time]
-    print("Moving run time results to new location")
-    for run, new_run in zip(run_time, new_run_time):
-        new_run.parent.mkdir(exist_ok=True, parents=True)
-        print(f"Moving {run} to {new_run}")
-        copy(run, new_run)
     print(f"Moving params file from {filename} to {path}")
     rename(filename, path / filename)
     print(f"Rendering plots in {path}/index.html")
+    subprocess.run(["dvc", "push"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     subprocess.run(["dvc", "plots", "show", "-o", path, "--html-template", "template.html"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     assert Path(path / "index.html").exists(), "Plots were not rendered"
     assert Path(path / filename).exists(), "Params was not saved"
