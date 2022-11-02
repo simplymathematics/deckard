0ecea0d2ef5c61c44bb9cb3cc70714a87b82e57b
Charlie Meyers <cmeyers@cs.umu.se>
2022-11-02 02:35:26 +0100

    +logistic driver

---
+++ examples/tikho/logistic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dvc exp run --set-param data.add_noise.X_train=.001,.01,.1,1,10,100 --set-param model.params.learning_rate=1e-12,1e-10,1e-9,1e-8,1e-7 --set-param model.params.mtype=logistic --queue
+dvc exp run --set-param model.params.scale=.001,.01,.1,1,10,100 --set-param model.params.learning_rate=1e-12,1e-10,1e-9,1e-8,1e-7 --set-param model.params.mtype=logistic --queue
+dvc exp run --set-param model.params.scale=.001,.01,.1,10,100 --set-param model.params.mtype=logistic --set-param
+data.add_noise.X_test=.001,.01,.1,1,10,100 --set-param model.params.learning_rate=1e-12,1e-10,1e-9,1e-8,1e-7 --set-param model.params.mtype=logistic --queue
+dvc queue status > queue.txt
+dvc queue start --jobs 32
