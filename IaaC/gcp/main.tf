e2920381906192efb37e64dee840a5b6a41dfcb7
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2023-06-24 14:11:06 +0200

    add networks and firewall rules for K8s

--- IaaC/gcp/main.tf
+++ IaaC/gcp/main.tf
@@ -14,3 +14,68 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
+
+resource "google_compute_network" "network" {
+  name = "decard-network"
+}
+
+resource "google_compute_subnetwork" "subnetwork" {
+  name          = "decard-subnetwork"
+  ip_cidr_range = "172.16.0.0/28"
+  region        = var.region
+  network       = google_compute_network.network.id
+}
+
+resource "google_compute_firewall" "network-fw-1" {
+  name    = "firewall-rules-for-k8s-api"
+  network = google_compute_network.network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["6443"]
+  }
+
+  direction = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "network-fw-2" {
+  name    = "firewall-rules-for-k8s-ssh"
+  network = google_compute_network.network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  direction = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "network-fw-3" {
+  name    = "firewall-rules-for-k8s-prometheus"
+  network = google_compute_network.network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["30090", "30091"]
+  }
+
+  direction = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "network-fw-4" {
+  name    = "firewall-rules-for-k8s-cilium"
+  network = google_compute_network.network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8472"]
+  }
+
+  direction = "INGRESS"
+  source_ranges = ["172.16.0.0/28"]
+}
+
+
