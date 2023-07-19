017fea45f66a6509ffecf89c5e15eca827e62e80
Charlie Meyers <cmeyers@cs.umu.se>
2023-07-19 12:05:01 +0000

    linting

--- test/base/test_model/test_art_pipeline.py
+++ test/base/test_model/test_art_pipeline.py
@@ -19,7 +19,8 @@ class testArtPipeline(unittest.TestCase):
 
     def setUp(self):
         with initialize_config_dir(
-            config_dir=Path(self.config_dir).resolve().as_posix(), version_base="1.3",
+            config_dir=Path(self.config_dir).resolve().as_posix(),
+            version_base="1.3",
         ):
             cfg = compose(config_name=self.config_file)
         self.cfg = cfg
