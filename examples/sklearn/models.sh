cca5dd79bacc8160fab3b8d7996d96f5f58f385a
simplymathematics <15224968+simplymathematics@users.noreply.github.com>
2023-09-07 01:03:29 +0200

    update sklearn example

--- examples/sklearn/models.sh
+++ examples/sklearn/models.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # This script is used to generate the models for the sklearn example.
-python -m ++data=small,medium,large,very_large ++model=linear  ++data.sample.random_state=0,1,2,3,4,5 ++data.init.C=.00001,.0001,.01,.1,1,10,1000,1000000 $@
+python -m deckard.layers.optimise  model=linear  ++data.sample.random_state=0,1,2,3,4,5 ++model.init.C=.00001,.0001,.01,.1,1,10,1000,1000000 $@
 
-python -m ++data=small,medium,large,very_large ++model=poly ++data.sample.random_state=0,1,2,3,4,5 ++data.init.C=.00001,.0001,.01,.1,1,10,1000,1000000 ++data.init.degree=1,2,3,4,5 ++data.init.coef0=.00001,.0001,.01,.1,1,10,1000,1000000 $@
+python -m deckard.layers.optimise ++data=small,medium,large,very_large model=poly ++data.sample.random_state=0,1,2,3,4,5 ++model.init.C=.00001,.0001,.01,.1,1,10,1000,1000000 ++data.init.degree=1,2,3,4,5 ++model.init.Coef0=.00001,.0001,.01,.1,1,10,1000,1000000 $@
 
-python -m ++data=small,medium,large,very_large ++model=rbf ++data.sample.random_state=0,1,2,3,4,5 ++data.init.C=.00001,.0001,.01,.1,1,10,1000,1000000 $@
+python -m deckard.layers.optimise ++data=small,medium,large,very_large model=rbf ++data.sample.random_state=0,1,2,3,4,5 ++model.init.C=.00001,.0001,.01,.1,1,10,1000,1000000 $@
