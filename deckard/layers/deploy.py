255a7f6efedc262728184c279a382529fdf17319
simplymathematics <15224968+simplymathematics@users.noreply.github.com>
2023-09-07 22:27:33 +0200

    Add iaac (#119)
    + GCP deployment
    + Docker Tests

--- deckard/layers/deploy.py
+++ deckard/layers/deploy.py
@@ -6,11 +6,11 @@ from ..iaac import GCP_Config
 
 
 logger = logging.getLogger(__name__)
-logging = logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 if __name__ == "__main__":
     iaac_parser = argparse.ArgumentParser()
     iaac_parser.add_argument("--verbosity", type=str, default="INFO")
-    iaac_parser.add_argument("--config_dir", type=str, default="conf")
+    iaac_parser.add_argument("--config_dir", type=str, default="conf/deploy")
     iaac_parser.add_argument("--config_file", type=str, default="default.yaml")
     iaac_parser.add_argument("--workdir", type=str, default=".")
     args = iaac_parser.parse_args()
