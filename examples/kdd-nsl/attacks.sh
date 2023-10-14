4d73ad4be82f05c96e9d633d1ea7f02cfc44a4eb
Charlie Meyers <cmeyers@cs.umu.se>
2023-10-15 00:56:45 +0200

    add kdd-nsl data

--- examples/kdd-nsl/attacks.sh
+++ examples/kdd-nsl/attacks.sh
@@ -1,40 +1,28 @@
 #!/bin/bash
-
-
-# while getops m:d:n:s:b: flag
-# do 
-#     case "${flag}" in
-#         m) model=(-m ${OPTARG});;
-#         d) attack_distance=(-d ${OPTARG});;
-#         n) attack_norm=(-n ${OPTARG});;
-#         s) attack_step=(${OPTARG});;
-#         b) attack_batch=(${OPTARG});;
-#     esac
-# done
-
-# model=$1
-# Pauses the script until the user presses enter
 MODEL_CONFIGS=$(ls conf/model/best_*.yaml)
 CONFIG_NAMES=$(ls conf/model/best_*.yaml | cut -d'/' -f3 | cut -d'.' -f1)
 TOTAL=$(( ${#CONFIG_NAMES[@]} ))
 i=$(( 0 ))
 mkdir -p logs/attacks/
 for model_config in $CONFIG_NAMES; do
+    kernel_name=$(echo $model_config | cut -d'_' -f2)
     i=$(( i + 1 ))
     if [ $model_config == "default" ]; then
         continue
     fi
-    echo "Running model $model_config. Number $i of $TOTAL" >> attack_log.txt
     HYDRA_FULL_ERROR=1 python -m deckard.layers.optimise \
+    ++model.init.kernel=kernel_name \
     ++stage=attack \
     ++attack.init.name=art.attacks.evasion.ProjectedGradientDescent \
     ++attack.init.norm=1,2,inf \
     ++attack.init.eps_step=.001,.01,.1,.3,.5,1 \
     ++attack.init.batch_size=1,10,50,100 \
     ++attack.init.eps=.001,.01,.1,.3,.5,1 \
+    ++attack.init.max_iter=1,10,100,1000 \
     ++hydra.sweeper.study_name=$model_config \
+    ++attack.attack_size=100 \
     model=$model_config $@ --multirun >> logs/attacks/$model_config.log
-    echo "Successfully completed model $model_config. Number $i of $TOTAL" >> attack_log.txt
+    echo "Successfully completed model $model_config" >> attack_log.txt
 done
 
 # Other attacks listed below
