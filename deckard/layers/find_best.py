02a3d347f7f1c9a868cc526f0dfca5bd35a317cb
Charlie Meyers <cmeyers@cs.umu.se>
2024-02-09 05:21:25 +0100

    cleaned up find_best_script

--- deckard/layers/find_best.py
+++ deckard/layers/find_best.py
@@ -3,6 +3,7 @@ import logging
 from pathlib import Path
 from hydra import initialize_config_dir, compose
 from omegaconf import OmegaConf
+import argparse
 import yaml
 from ..base.utils import flatten_dict
 
@@ -40,6 +41,41 @@ def find_optuna_best(
     logger.debug(f"Best user params: {more_params}")
     logger.debug(f"Best system params: {even_more_params}")
     best_params = {**more_params, **best_params}
+    cfg = override_default_with_best(config_folder, default_config, best_params)
+    params = cfg.get(config_subdir, cfg)
+    if params_file is not None:
+        params_file = create_new_config_in_subdir(params_file, config_folder, default_config, config_subdir, params)
+    return params
+
+def create_new_config_in_subdir(params_file, config_folder, default_config, config_subdir, params):
+    if params_file is True:
+        if config_subdir is not None:
+            params_file = Path(
+                    config_folder,
+                    f"{config_subdir}",
+                    f"{default_config}.yaml",
+                )
+        else:
+            params_file = Path(config_folder, f"{default_config}.yaml")
+    else:
+        if config_subdir is not None:
+            params_file = Path(
+                    config_folder,
+                    f"{config_subdir}",
+                    f"{params_file}.yaml",
+                )
+        else:
+            params_file = Path(config_folder, f"{params_file}.yaml")
+    params_file.parent.mkdir(parents=True, exist_ok=True)
+    with open(params_file.with_suffix(".yaml"), "w") as f:
+        yaml.dump(params, f)
+    assert (
+            params_file.exists()
+        ), f"{params_file.resolve().as_posix()} does not exist."
+    
+    return params_file
+
+def override_default_with_best(config_folder, default_config, best_params):
     overrides = []
     for key, value in best_params.items():
         logger.info(f"Overriding {key} with {value}")
@@ -47,45 +83,14 @@ def find_optuna_best(
     with initialize_config_dir(config_dir=config_folder, version_base="1.3"):
         cfg = compose(config_name=default_config, overrides=overrides)
     cfg = OmegaConf.to_container(cfg, resolve=False)
-    cfg = cfg.get(config_subdir, cfg)
-    if params_file is not None:
-        if params_file is True:
-            if config_subdir is not None:
-                params_file = Path(
-                    config_folder,
-                    f"{config_subdir}",
-                    f"{default_config}.yaml",
-                )
-                params = cfg.get(config_subdir)
-            else:
-                params_file = Path(config_folder, f"{default_config}.yaml")
-                params = cfg
-        else:
-            if config_subdir is not None:
-                params_file = Path(
-                    config_folder,
-                    f"{config_subdir}",
-                    f"{params_file}.yaml",
-                )
-                params = cfg.get(config_subdir)
-            else:
-                params = cfg
-                params_file = Path(config_folder, f"{params_file}.yaml")
-        params_file.parent.mkdir(parents=True, exist_ok=True)
-        with open(params_file.with_suffix(".yaml"), "w") as f:
-            yaml.dump(params, f)
-        assert (
-            params_file.exists()
-        ), f"{params_file.resolve().as_posix()} does not exist."
-    return params
+    return cfg
 
 
 if __name__ == "__main__":
-    import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--params_file", type=str, default=True)
-    parser.add_argument("--study_type", type=str, default="optuna")
+    
     parser.add_argument("--study_csv", type=str, default=None)
     parser.add_argument("--config_folder", type=str, default=Path(Path(), "conf"))
     parser.add_argument("--default_config", type=str, default="default")
@@ -93,26 +98,22 @@ if __name__ == "__main__":
     parser.add_argument("--study_name", type=str, required=True)
     parser.add_argument("--config_name", type=str)
     parser.add_argument("--verbosity", type=str, default="INFO")
+    parser.add_argument("--storage_name", type=str, required=True)
+    parser.add_argument("--direction", type=str, default="maximize")
+    parser.add_argument("--study_type", type=str, default="optuna")
     args = parser.parse_args()
 
     args.config_folder = Path(args.config_folder).resolve().as_posix()
-
+    logging
     if args.study_type == "optuna":
-        with initialize_config_dir(config_dir=args.config_folder, version_base="1.3"):
-            default_params = compose(config_name=args.default_config, return_hydra_config=True, overrides=["++hydra.job.num=0", "++hydra.job_logging.handlers.file.filename=null"])
-        default_params = OmegaConf.to_container(OmegaConf.create(default_params), resolve=True)
-        if "hydra" in default_params:
-            hydra_params = default_params.pop("hydra")
-        else:
-            raise ValueError("No hydra params found in default config.")
         study_name = args.study_name
-        storage_name = hydra_params["sweeper"]["storage"]
-        direction = default_params.get("direction", "maximize")
+        storage_name = args.storage_name
+        direction = args.direction
         if len(direction) == 1:
             direction = direction[0]
         find_optuna_best(
-            study_name=study_name,
-            storage_name=storage_name,
+            study_name=args.study_name,
+            storage_name=args.storage_name,
             study_csv=args.study_csv,
             params_file=args.params_file,
             config_folder=args.config_folder,
