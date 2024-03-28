26d1bb1cc7a8c6e7defb1e4782391ffab9fe725b
Charlie Meyers <cmeyers@cs.umu.se>
2024-03-28 03:58:12 +0100

    linting

--- deckard/layers/afr.py
+++ deckard/layers/afr.py
@@ -324,7 +324,7 @@ def render_afr_plot(
             ),
             file=config.get("file", f"{mtype}_aft.pdf"),
             xlabel=label_dict.get("xlabel", "Time (s)"),
-            ylabel=label_dict.get("ylabel", "$\mathcal{P}~(T>t)$"), # noqa W605
+            ylabel=label_dict.get("ylabel", "$\mathcal{P}~(T>t)$"),  # noqa W605
             replacement_dict=label_dict,
             folder=folder,
         )
