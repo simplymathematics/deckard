f050424da9be6126a509a055681032c42c09eb39
Charlie Meyers <cmeyers@cs.umu.se>
2024-03-30 00:27:55 +0100

    fix sample size bug

--- examples/power/plots/combined_plots.py
+++ examples/power/plots/combined_plots.py
@@ -76,23 +76,23 @@ big_df["adv_fit_cost"] = big_df["adv_fit_time"] * big_df["cost"]
 
 ben_train_samples = 48000
 ben_pred_samples = 12000
-adv_pred_samples = big_df['attack.attack_size'].values
-train_cost_per_sample = big_df['train_cost'] / ben_train_samples
-predict_cost_per_sample = big_df['predict_cost'] / ben_pred_samples
-adv_fit_cost_per_sample = big_df['adv_fit_cost'] / adv_pred_samples
-train_power_per_sample = big_df['train_power'] / ben_train_samples
-predict_power_per_sample = big_df['predict_power'] / ben_pred_samples
-adv_fit_power_per_sample = big_df['adv_fit_power'] / adv_pred_samples
+adv_pred_samples = big_df["attack.attack_size"].values
+train_cost_per_sample = big_df["train_cost"] / ben_train_samples
+predict_cost_per_sample = big_df["predict_cost"] / ben_pred_samples
+adv_fit_cost_per_sample = big_df["adv_fit_cost"] / adv_pred_samples
+train_power_per_sample = big_df["train_power"] / ben_train_samples
+predict_power_per_sample = big_df["predict_power"] / ben_pred_samples
+adv_fit_power_per_sample = big_df["adv_fit_power"] / adv_pred_samples
 big_df = big_df.reset_index(drop=True)
-big_df['train_cost_per_sample'] = train_cost_per_sample.values
-big_df['predict_cost_per_sample'] = predict_cost_per_sample.values
-big_df['adv_fit_cost_per_sample'] = adv_fit_cost_per_sample.values
-big_df['train_power_per_sample'] = train_power_per_sample.values
-big_df['predict_power_per_sample'] = predict_power_per_sample.values
-big_df['adv_fit_power_per_sample'] = adv_fit_power_per_sample.values
-big_df['train_time_per_sample'] = big_df['train_time'] / ben_train_samples
-big_df['predict_time_per_sample'] = big_df['predict_time'] / ben_pred_samples
-big_df['adv_fit_time_per_sample'] = big_df['adv_fit_time'] / adv_pred_samples
+big_df["train_cost_per_sample"] = train_cost_per_sample.values
+big_df["predict_cost_per_sample"] = predict_cost_per_sample.values
+big_df["adv_fit_cost_per_sample"] = adv_fit_cost_per_sample.values
+big_df["train_power_per_sample"] = train_power_per_sample.values
+big_df["predict_power_per_sample"] = predict_power_per_sample.values
+big_df["adv_fit_power_per_sample"] = adv_fit_power_per_sample.values
+big_df["train_time_per_sample"] = big_df["train_time"] / ben_train_samples
+big_df["predict_time_per_sample"] = big_df["predict_time"] / ben_pred_samples
+big_df["adv_fit_time_per_sample"] = big_df["adv_fit_time"] / adv_pred_samples
 
 big_df["Device"] = big_df["device_id"].str.replace("-", " ").str.title()
 big_df = big_df.reset_index(drop=True)
@@ -102,9 +102,9 @@ big_df.to_csv("data/combined/combined.csv")
 big_df = pd.read_csv("data/combined/combined.csv", index_col=0, low_memory=False)
 
 # Capitalize all letters in the dataset
-big_df['dataset'] = big_df['dataset'].str.upper()
+big_df["dataset"] = big_df["dataset"].str.upper()
 # Split the device_id on the last word and only keep the last word
-big_df['Device'] = big_df['device_id'].str.split('-').str[-1].str.upper()
+big_df["Device"] = big_df["device_id"].str.split("-").str[-1].str.upper()
 # Accuracy Plot
 fig, ax = plt.subplots(1, 2, figsize=(8, 3))
 ben_acc = sns.boxenplot(
@@ -117,7 +117,7 @@ ben_acc = sns.boxenplot(
 ben_acc.set_title("")
 ben_acc.set_ylabel("Ben. Accuracy")
 ben_acc.set_xlabel("")
-ben_acc.tick_params(axis='x', labelsize=12, rotation=0)
+ben_acc.tick_params(axis="x", labelsize=12, rotation=0)
 ben_acc.set_yscale("linear")
 ben_acc.legend().remove()
 adv_acc = sns.boxenplot(
@@ -131,7 +131,7 @@ adv_acc.set_title("")
 adv_acc.set_ylabel("Adv. Accuracy")
 adv_acc.set_yscale("linear")
 adv_acc.set_xlabel("")
-adv_acc.tick_params(axis='x', labelsize=12, rotation=0)
+adv_acc.tick_params(axis="x", labelsize=12, rotation=0)
 xticklabels = [item.get_text() for item in adv_acc.get_xticklabels()]
 adv_acc.legend()
 # for _, ax in enumerate(fig.axes):
@@ -155,7 +155,7 @@ train_time = sns.boxenplot(
 train_time.set_title("")
 train_time.set_ylabel("$t_{t}$ (seconds)")
 train_time.set_xlabel("")
-train_time.tick_params(axis='x', labelsize=18)
+train_time.tick_params(axis="x", labelsize=18)
 train_time.legend().remove()
 predict_time = sns.boxenplot(
     data=big_df,
@@ -167,7 +167,7 @@ predict_time = sns.boxenplot(
 predict_time.set_title("")
 predict_time.set_ylabel("$t_{i}$ (seconds)")
 predict_time.set_xlabel("")
-predict_time.tick_params(axis='x', labelsize=18)
+predict_time.tick_params(axis="x", labelsize=18)
 predict_time.legend().remove()
 adv_fit_time = sns.boxenplot(
     data=big_df,
@@ -179,7 +179,7 @@ adv_fit_time = sns.boxenplot(
 adv_fit_time.set_title("")
 adv_fit_time.set_ylabel("$t_{a}$ (seconds)")
 adv_fit_time.set_xlabel("")
-adv_fit_time.tick_params(axis='x', labelsize=18)
+adv_fit_time.tick_params(axis="x", labelsize=18)
 adv_fit_time.legend()
 fig.tight_layout()
 fig.savefig("combined/time.pdf")
@@ -196,7 +196,7 @@ train_power = sns.boxenplot(
 train_power.set_title("")
 train_power.set_ylabel("$P_{t}$ (Watts)")
 train_power.set_xlabel("")
-train_power.tick_params(axis='x', labelsize=18)
+train_power.tick_params(axis="x", labelsize=18)
 train_power.legend().remove()
 predict_power = sns.boxenplot(
     data=big_df,
@@ -208,7 +208,7 @@ predict_power = sns.boxenplot(
 predict_power.set_title("")
 predict_power.set_ylabel("$P_{i}$ (Watts)")
 predict_power.set_xlabel("")
-predict_power.tick_params(axis='x', labelsize=18)
+predict_power.tick_params(axis="x", labelsize=18)
 predict_power.legend().remove()
 adv_fit_power = sns.boxenplot(
     data=big_df,
@@ -220,7 +220,7 @@ adv_fit_power = sns.boxenplot(
 adv_fit_power.set_title("")
 adv_fit_power.set_ylabel("$P_{a}$ (Watts)")
 adv_fit_power.set_xlabel("")
-adv_fit_power.tick_params(axis='x', labelsize=18)
+adv_fit_power.tick_params(axis="x", labelsize=18)
 adv_fit_power.legend()
 fig.tight_layout()
 fig.savefig("combined/power.pdf")
@@ -237,7 +237,7 @@ train_cost = sns.boxenplot(
 train_cost.set_title("")
 train_cost.set_ylabel("$C_{t}$ (USD)")
 train_cost.set_xlabel("")
-train_cost.tick_params(axis='x', labelsize=18)
+train_cost.tick_params(axis="x", labelsize=18)
 train_cost.legend().remove()
 predict_cost = sns.boxenplot(
     data=big_df,
@@ -249,7 +249,7 @@ predict_cost = sns.boxenplot(
 predict_cost.set_title("")
 predict_cost.set_ylabel("$C_{i}$ (USD)")
 predict_cost.set_xlabel("")
-predict_cost.tick_params(axis='x', labelsize=18)
+predict_cost.tick_params(axis="x", labelsize=18)
 predict_cost.legend().remove()
 adv_fit_cost = sns.boxenplot(
     data=big_df,
@@ -261,7 +261,7 @@ adv_fit_cost = sns.boxenplot(
 adv_fit_cost.set_title("")
 adv_fit_cost.set_ylabel("$C_{a}$ (USD)")
 adv_fit_cost.set_xlabel("")
-adv_fit_cost.tick_params(axis='x', labelsize=18)
+adv_fit_cost.tick_params(axis="x", labelsize=18)
 adv_fit_cost.legend()
 fig.tight_layout()
 fig.savefig("combined/cost.pdf")
