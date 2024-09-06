f82995a6904a299d1ea2288e1e73c1428e5f83f2
Charlie Meyers <cmeyers@cs.umu.se>
2024-09-06 02:04:56 +0200

    linting

--- deckard/base/data/data.py
+++ deckard/base/data/data.py
@@ -183,7 +183,10 @@ class Data:
                     raise ValueError(f"Unknown data type {type(data)} for {filename}.")
                 try:
                     DataFrame(data).to_json(
-                        filename, index=False, force_ascii=False, indent=4
+                        filename,
+                        index=False,
+                        force_ascii=False,
+                        indent=4,
                     )
                 except ValueError as e:
                     if "using all scalar values" in str(e):
@@ -191,7 +194,10 @@ class Data:
                         data = dict(sorted(data.items()))
                         # Save the dictionary to a JSON file
                         Series(data).to_json(
-                            filename, index=False, force_ascii=False, indent=4
+                            filename,
+                            index=False,
+                            force_ascii=False,
+                            indent=4,
                         )
                     else:
                         raise e
