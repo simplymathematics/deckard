54ec18c5fe9ae776d03a4c277f0f4c1b6cfc6945
simplymathematics <15224968+simplymathematics@users.noreply.github.com>
2024-12-02 04:42:56 +0100

    Merge branches (#187)
    gzip example, new build chain

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sudo apt-get update &&  sudo apt-get install python3 python3-pip -y
 sudo apt-get upgrade -y
-# git submodule update --init --recursive
-python3 -m pip install  . --verbose # install deckard
+python3 -m pip install --upgrade pip
+python3 -m pip install  -e . --verbose # install deckard
