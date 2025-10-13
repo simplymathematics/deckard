4e0b2914acdec01c6d0badc5f49395a567e70d64
simplymathematics <simplymathematics@users.noreply.github.com>
2025-10-12 20:41:27 -0400

    moved attack to subfolder

--- test/test_attack.py
+++ test/test_attack.py
@@ -1,7 +1,21 @@
 import pandas as pd
 import numpy as np
 from deckard.attack import AttackConfig, attack_defaults
+class DummyEstimator:
+        def predict(self, X):
+            return np.zeros((len(X), 2))
 
+class DummyData:
+    def __call__(self, *args, **kwargs):
+        X_train = pd.DataFrame(np.random.rand(20, 3), columns=["a", "b", "c"])
+        y_train = pd.Series([0] * 10 + [1] * 10)
+        X_test = pd.DataFrame(np.random.rand(20, 3), columns=["a", "b", "c"])
+        y_test = pd.Series([0] * 10 + [1] * 10)
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        return self
 
 def test_attackconfig_default_initialization():
     config = AttackConfig()
@@ -9,7 +23,7 @@ def test_attackconfig_default_initialization():
     assert config.attack_name == "art.attacks.evasion.HopSkipJump"
     assert config.attack_size == 10
     assert config._attack is None
-    assert isinstance(config._score_dict, dict)
+    assert isinstance(config.score_dict, dict)
 
 
 def test_attackconfig_hash_and_post_init():
@@ -20,7 +34,7 @@ def test_attackconfig_hash_and_post_init():
     h = hash(config)
     assert isinstance(h, int)
     assert config._attack is None
-    assert isinstance(config._score_dict, dict)
+    assert isinstance(config.score_dict, dict)
 
 
 def test_attack_defaults_keys():
@@ -44,13 +58,13 @@ def test_attackconfig_pop_attribute_removes_column():
     assert all(col == pd.Series([1, 2, 3]))
 
 
-def test_attackconfig_score_attack_sets_score_dict():
+def test_attackconfig_score_attack_setsscore_dict():
     config = AttackConfig()
     ben_pred = np.array([0, 1, 1, 0])
     adv_pred = np.array([1, 1, 0, 0])
     y_true = np.array([0, 1, 1, 0])
     config._score_attack(ben_pred, adv_pred, y_true)
-    assert isinstance(config._score_dict, dict)
+    assert isinstance(config.score_dict, dict)
     for key in [
         "adversarial_accuracy",
         "adversarial_precision",
@@ -58,25 +72,15 @@ def test_attackconfig_score_attack_sets_score_dict():
         "adversarial_f1-score",
         "adversarial_success_rate",
     ]:
-        assert key in config._score_dict
+        assert key in config.score_dict
 
 
 def test_attackconfig_get_benign_preds_shape():
-    class DummyEstimator:
-        def predict(self, X):
-            return np.zeros((len(X), 2))
-
-    class DummyData:
-        def __call__(self, *args, **kwargs):
-            X_train = pd.DataFrame(np.random.rand(20, 3), columns=["a", "b", "c"])
-            y_train = pd.Series([0] * 10 + [1] * 10)
-            X_test = pd.DataFrame(np.random.rand(20, 3), columns=["a", "b", "c"])
-            y_test = pd.Series([0] * 10 + [1] * 10)
-            return X_train, y_train, X_test, y_test
+    
 
     config = AttackConfig(attack_size=5)
     n, ben_pred_labels, X_subset, y_subset = config._get_benign_preds(
-        DummyData(),
+        DummyData()(),
         DummyEstimator(),
         train=False,
     )
@@ -87,19 +91,10 @@ def test_attackconfig_get_benign_preds_shape():
 
 
 def test_attackconfig_get_feature_vector_preds_shape():
-    class DummyData:
-        def __call__(self):
-            X_train = pd.DataFrame(np.random.rand(20, 3), columns=["a", "b", "c"])
-            y_train = pd.Series([0] * 10 + [1] * 10)
-            a_train = pd.Series(["x"] * 10 + ["y"] * 10)
-            X_test = pd.DataFrame(np.random.rand(20, 3), columns=["a", "b", "c"])
-            y_test = pd.Series([0] * 10 + [1] * 10)
-            a_test = pd.Series(["x"] * 10 + ["y"] * 10)
-            return X_train, y_train, a_train, X_test, y_test, a_test
 
     config = AttackConfig(attack_size=5)
     n, X_subset, y_subset, a_subset = config._get_feature_vector_preds(
-        DummyData(),
+        DummyData()(),
         "a",
         train=False,
     )
