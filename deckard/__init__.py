a04cbc8da8f26058ec949d5ec6e3dda5dab360cc
simplymathematics <simplymathematics@users.noreply.github.com>
2025-10-12 20:26:05 -0400

    filter some warnings

--- deckard/__init__.py
+++ deckard/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import warnings
 from .data import *
 from .model import *
 from .attack import *
@@ -6,7 +7,13 @@ from .data import DataConfig, data_parser, data_main
 from .model import ModelConfig, model_parser, model_main
 from .attack import AttackConfig, attack_parser, attack_main
 
-# Import configs, parsers, and mains from each module
+# Suppress UserWarnings from sklearn
+warnings.filterwarnings("ignore", category=UserWarning)
+# Suppress RuntimeWarnings from numpy
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+# Suppress ConvergenceWarnings from sklearn
+from sklearn.exceptions import ConvergenceWarning
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 # Set up logging
 logging.basicConfig(
