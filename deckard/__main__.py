ca3eecdcc360bab21d49c293e77abf64c88b7256
simplymathematics <simplymathematics@users.noreply.github.com>
2025-10-22 13:21:15 -0400

    make main a hydra script

--- deckard/__main__.py
+++ deckard/__main__.py
@@ -1,187 +1,73 @@
-import argparse
 import warnings
 import logging
 import os
 import sys
 from pathlib import Path
 
-from hydra import initialize_config_dir
+from omegaconf import OmegaConf
+import hydra
+from hydra.utils import instantiate
 
 
 from .experiment import ExperimentConfig
-from .file import FileConfig
-from .utils import create_parser_from_function, initialize_config
+
 
 logger = logging.getLogger(__name__)
 
-# Get env variables for logging level, default to INFO
-log_level = os.getenv("DECKARD_LOG_LEVEL", "INFO").upper()
-logging.basicConfig(level=log_level, format="%(asctime)s - %(levelname)s - %(message)s")
 
-# Suppress specific warnings
+# Suppress sklearn runtime warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning, module="sklearn")
 
+supported_modules = ["data", "model", "defend", "attack"]
+# Parse the config_dir argument first to set up config dir
+
+
+def optimize(cfg: ExperimentConfig) -> None:
+    cfg = OmegaConf.to_container(cfg, resolve=True)
+    # Pop "optimizers" from cfg if in cfg and use it to return a subset of the scores
+    if "optimizers" in cfg and cfg.optimizers is not None:
+        optimizers = cfg.optimizers
+        cfg.pop("optimizers")
+    else:
+        optimizers = None
+    # Initialize experiment config
+    if "_target_" not in cfg:
+        cfg["_target_"] = "deckard.experiment.ExperimentConfig"
+    experiment = instantiate(cfg)
+    scores = experiment()
+    if optimizers:
+        scores = {k: v for k, v in scores.items() if k in optimizers}
+    return scores
 
 
 def main():
-    init_parser = argparse.ArgumentParser(
-        description="Deckard Experiment Initialization",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    # Get config dir from environment variable if set
+    config_dir = os.environ.get(
+        "DECKARD_CONFIG_DIR",
+        ValueError("DECKARD_CONFIG_DIR environment variable not set."),
     )
-    
-    init_parser.add_argument_group("Experiment Initialization Arguments", "Experiment initialization parameters.")
-    # init_parser = create_parser_from_function(ExperimentConfig.__init__, init_parser)
-    init_parser.add_argument(
-        "--experiment_name",
-        type=str,
-        required=True,
-        help="Name of the experiment.",
-    )
-    init_parser.add_argument(
-        "--config_dir",
-        type=str,
-        default="config",
-        help="Directory for configuration files.",
-        
-    )
-    init_parser.add_argument(
-        "--files",
-        nargs="+",
-        type=str,
-        help="Additional file parameters to override in the format key=value.",
-        metavar="key=value",
-        default=None,
-    )
-    # Data config
-    data_args = init_parser.add_argument_group("Data Configuration", "Parameters for data configuration.")
-    data_args.add_argument(
-        "--data_config_file",
-        type=str,
-        required=True,
-        help="Path to the data configuration YAML file.",
-    )
-    data_args.add_argument(
-        "--data_config_dir",
-        type=str,
-        default="data",
-        help="Directory for data configuration files.",
-    )
-    
-    # Model config
-    model_args = init_parser.add_argument_group("Model Configuration", "Parameters for model configuration.")
-    model_args.add_argument(
-        "--model_config_file",
-        type=str,
-        required=False,
-        default=None,
-        help="Path to the model configuration YAML file.",
-    )
-    model_args.add_argument(
-        "--model_config_dir",
-        type=str,
-        default="model",
-        help="Directory for model configuration files.",
-    )
-    # Defense config
-    defense_args = init_parser.add_argument_group("Defense Configuration", "Parameters for defense configuration.")
-    defense_args.add_argument(
-        "--defense_config_file",
-        type=str,
-        required=False,
-        default=None,
-        help="Path to the defense configuration YAML file.",
-    )
-    defense_args.add_argument(
-        "--defense_config_dir",
-        type=str,
-        default="defend",
-        help="Directory for defense configuration files.",
-    )
-    
-    # Attack config
-    attack_args = init_parser.add_argument_group("Attack Configuration", "Parameters for attack configuration.")
-    attack_args.add_argument(
-        "--attack_config_file",
-        type=str,
-        required=False,
-        default=None,
-        help="Path to the attack configuration YAML file.",
-    )
-    attack_args.add_argument(
-        "--attack_config_dir",
-        type=str,
-        default="attack",
-        help="Directory for attack configuration files.",
-    )
+    config_dir = str(Path(config_dir).resolve())
+    config_file = os.environ.get("DECKARD_DEFAULT_CONFIG_FILE", "default.yaml")
+    logger.info(f"Working directory: {os.getcwd()}")
+    logger.info("Starting Deckard with Hydra configuration.")
+    logger.info(f"Config directory: {Path(config_dir).resolve()}")
+    config_file = Path(config_dir) / config_file
+    logger.info(f"Resolved config file path: {config_file.resolve()}")
+    if not config_file.exists():
+        logger.error(f"Config file {config_file} does not exist.")
+        sys.exit(1)
 
-    args = init_parser.parse_args()
-    working_dir = os.getcwd()
-    # Initialize config directory
-    # Assert that args.config_dir exists
-    assert Path(args.config_dir).exists(), f"Config directory {args.config_dir} does not exist."
-    config_dir = str(Path(args.config_dir).resolve())
-    initialize_config_dir(config_dir)
+    @hydra.main(
+        config_path=config_dir, config_name=str(config_file.name), version_base="1.3"
+    )
+    def main_hydra(cfg: ExperimentConfig) -> None:
+        scores = optimize(cfg=cfg)
 
-    logger.info(f"Current working directory: {working_dir}")
-    for group in init_parser._action_groups:
-        if group.title == "Data Configuration":
-            data_args = group
-            data_config_file = getattr(args, "data_config_file")
-            # Assume ./config_dir/data_config_dir/data_config_file
-            config_file = Path(getattr(args, "data_config_dir")) / data_config_file
-            logger.info(f"Loading data_config from resolved path: {config_file.resolve()}")
-            if config_file.is_absolute():
-                # Use os to make this relative to working dir
-                config_file = Path(os.path.relpath(config_file, working_dir))
-                logger.info(f"Converted absolute config_file to relative path: {config_file}")
-            data = initialize_config(config_file=config_file, params={}, target="deckard.data.DataConfig")
-        elif group.title == "Model Configuration":
-            model_args = group
-            model_config_file = getattr(args, "model_config_file")
-            if model_config_file is None:
-                model = None
-                continue
-            config_file = Path(getattr(args, "model_config_dir")) / model_config_file
-            logger.info(f"Loading model_config from resolved path: {config_file.resolve()}")
-            if config_file.is_absolute():
-                # Use os to make this relative to working dir
-                config_file = Path(os.path.relpath(config_file, working_dir))
-                logger.info(f"Converted absolute config_file to relative path: {config_file}")
-            model = initialize_config(config_file=config_file, params={}, target="deckard.model.ModelConfig")
-        elif group.title == "Defense Configuration":
-            defense_args = group
-            defense_config_file = getattr(args, "defense_config_file")
-            if defense_config_file is None:
-                defense = None
-                continue
-            config_file = Path(getattr(args, "defense_config_dir")) / defense_config_file
-            logger.info(f"Loading defense_config from resolved path: {config_file.resolve()}")
-            defense = initialize_config(config_file=config_file, params={}, target="deckard.defense.DefenseConfig")
-            model = defense
-        elif group.title == "Attack Configuration":
-            attack_args = group
-            attack_config_file = getattr(args, "attack_config_file")
-            if attack_config_file is None:
-                attack = None
-                continue
-            config_file = Path(getattr(args, "attack_config_dir")) / attack_config_file
-            logger.info(f"Loading attack_config from resolved path: {config_file.resolve()}")
-            attack = initialize_config(config_file=config_file, params={}, target="deckard.attack.AttackConfig")
-    # File config
-    files = FileConfig(
-        **vars(args).copy()
-    )
-    files = files(**{k.split("=")[0]: v for k, v in (arg.split("=") for arg in args.files)}) if args.files else files()
-    experiment = ExperimentConfig(
-        experiment_name=args.experiment_name,
-        data=data,
-        model=model,
-        attack=attack,
-        files=files,
-    )
-    scores = experiment(**files)
+        return scores
+
+    return main_hydra()
+
+
 if __name__ == "__main__":
+
     main()
-
-
-
