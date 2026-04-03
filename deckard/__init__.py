af5edb444f663e074897a9a4ddfbece37a20f944
simplymathematics <simplymathematics@users.noreply.github.com>
2026-04-03 15:57:12 -0400

    better logging

--- deckard/__init__.py
+++ deckard/__init__.py
@@ -1,4 +1,9 @@
 import logging
+import os
+from pathlib import Path
+import warnings
+from sklearn.exceptions import UndefinedMetricWarning
+
 from .data import DataConfig
 from .model import ModelConfig
 from .model.defend import DefenseConfig
@@ -19,3 +24,41 @@ __all__ = [
     "FileConfig",
     "ScorerDictConfig",
 ]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "std": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M",
+        },
+    },
+    "handlers": {
+        "default": {
+            # Use RotatingFileHandler for log rotation
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": os.path.join(Path.cwd(), "deckard.log"),
+            "formatter": "std",
+            "level": logging.INFO,
+            "maxBytes": 10 * 1024 * 1024,  # 10 MB log file size limit
+            "backupCount": 5,  # Keep up to 5 backup files
+            "mode": "a",
+        },
+        "test": {
+            "class": "logging.StreamHandler",
+            "formatter": "std",
+            "level": logging.DEBUG,
+        },
+    },
+    "loggers": {
+        "deckard": {"handlers": ["default"], "level": "INFO", "propagate": True},
+        "tests": {"handlers": ["test"], "level": "DEBUG", "propagate": True},
+    },
+}
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
