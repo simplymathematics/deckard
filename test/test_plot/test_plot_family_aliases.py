ef6371ba5bd72e6e9ea27881dc060907208f65a5
simplymathematics <15224968+simplymathematics@users.noreply.github.com>
2026-05-19 02:38:52 -0400

    update paths, build ignores

--- test/test_plot/test_plot_family_aliases.py
+++ test/test_plot/test_plot_family_aliases.py
@@ -2,7 +2,7 @@ from deckard.plugins.lifelines.plot import SurvivalSeabornPlotterConfig
 from deckard.plugins.seaborn.plot import SeabornPlotConfig
 from deckard.plugins.yellowbrick.plot import YellowbrickPlotConfig
 from deckard.plot.yellowbrick_plots import YellowbrickPlotConfig as LegacyYellowbrickPlotConfig
-from deckard.data.fairness_pytorch import TinyFairness as LegacyTinyFairness
+from deckard.plugins.fairlearn.data_pytorch import TinyFairness as LegacyTinyFairness
 from deckard.frameworks.pytorch.fairness_data import TinyFairness
 from deckard.Anjana import AnjanaDataConfig as LegacyAnjanaDataConfig
 from deckard.plugins.anjana.data import AnjanaDataConfig
